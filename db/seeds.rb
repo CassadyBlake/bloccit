beadbbeb42c758a50c44f40e6b78e49ab5764779
CassadyBlake <cassady.lillstrom@gmail.com>
2018-04-05 14:54:25 -0700

    bloccit voting checkpoint: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,13 +35,17 @@ topics = Topic.all
 end
 
 50.times do
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
+
 posts = Post.all
 
 100.times do
@@ -97,5 +101,6 @@ puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
 puts "#{Advertisement.count} Ads created"
 puts "#{Question.count} Questions created"
