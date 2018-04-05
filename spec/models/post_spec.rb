67446e54ddd1dfab2022851b1255121c4e39643f
CassadyBlake <cassady.lillstrom@gmail.com>
2018-04-05 15:26:04 -0700

    bloccit voting assignment: completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -72,6 +72,22 @@ RSpec.describe Post, type: :model do
         post.votes.create!(value: -1, user: user)
         expect(post.rank).to eq (old_rank - 1)
       end
-    end 
+    end
+  end
+
+  describe "create_vote" do
+    it "sets up_votes to 1" do
+      expect(post.up_votes).to eq(1)
+    end
+
+    it "executes create_vote when new post is created" do
+      post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+      expect(post).to receive(:create_vote)
+      post.save
+    end
+
+    it "links the vote to the post creator" do
+      expect(post.votes.first.user).to eq(post.user)
+    end
   end
 end
